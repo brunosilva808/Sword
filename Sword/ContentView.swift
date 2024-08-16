84143bfcca4f68e3098e52e04b3620214a6855f8
Bruno Silva <bsimoesmail@gmail.com>
2024-08-16 10:37:50 +0100

    Project init

---
+++ Sword/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sword
+//
+//  Created by Bruno Silva on 16/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sword/SwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwordApp.swift
+//  Sword
+//
+//  Created by Bruno Silva on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
